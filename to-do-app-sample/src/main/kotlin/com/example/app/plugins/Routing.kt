0a0d02e91b8f77c9d6ea2dbf194bf6993ac33286
t-matsumo <tatsukimatsumo@gmail.com>
2023-07-22 22:33:32 +0900

    CRUDを雑に実装

--- to-do-app-sample/src/main/kotlin/com/example/app/plugins/Routing.kt
+++ to-do-app-sample/src/main/kotlin/com/example/app/plugins/Routing.kt
@@ -2,7 +2,14 @@ package com.example.app.plugins
 
 import com.example.app.todo.toDoRoutes
 import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
+    routing {
+        get {
+            call.respondRedirect("/todos")
+        }
+    }
     toDoRoutes()
 }
--- to-do-app-sample/src/main/kotlin/com/example/domain/todo/update/UpdateToDoUseCase.kt
+++ to-do-app-sample/src/main/kotlin/com/example/domain/todo/update/UpdateToDoUseCase.kt
@@ -1,6 +1,6 @@
 package com.example.domain.todo.update
 
-data class UpdateToDoRequest(val id: String, val content: String)
+data class UpdateToDoRequest(val id: String, val title: String, val content: String)
 interface UpdateToDoUseCase {
     fun handle(request: UpdateToDoRequest)
 }
